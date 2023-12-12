0f475b03eff3d0b3cede09744712cfff9735e759
Makktu <johnmcna@gmail.com>
2023-12-12 03:21:10 +0000

    adding screens and search

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,64 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  TextInput,
+  InputAccessoryView,
+} from 'react-native';
+import React from 'react';
+
+export default function SearchBox({ enteredText, inputHandler }) {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        maxLength={16}
+        autoCapitalize='characters'
+        autoCorrect={false}
+        value={enteredText}
+        onChangeText={inputHandler}
+        placeholder='Search'
+        placeholderTextColor='black'
+        keyboardAppearance='dark'
+      />
+      <Pressable style={styles.clearButton}>
+        <View>
+          <Text style={styles.clearText}>CLEAR</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    height: 70,
+    width: '76%',
+    fontSize: 26,
+    backgroundColor: '#eae0e0',
+    color: 'whitesmoke',
+    padding: 2,
+    color: 'black',
+    textAlign: 'center',
+    borderRadius: 4,
+  },
+  clearButton: {
+    width: '20%',
+    height: 70,
+    backgroundColor: 'red',
+    borderRadius: 4,
+    marginLeft: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  clearText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+});
--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,11 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, KeyboardAvoidingView, View } from 'react-native';
+import React, { useState } from 'react';
+import SearchBox from '../components/SearchBox';
+import { Keyboard } from 'react-native';
+
+const locationData = require('./../../data/locations.json');
 
 export default function SearchScreen() {
+  const [keyboardShowing, setKeyboardShowing] = useState(false);
+
+  Keyboard.addListener('keyboardDidHide', () => {
+    console.log('showing');
+    setKeyboardShowing(false);
+  });
+  Keyboard.addListener('keyboardDidShow', () => {
+    console.log('hiding');
+    setKeyboardShowing(true);
+  });
+
+  const inputHandler = () => {
+    console.log('this');
+  };
   return (
-    <View style={styles.container}>
-      <Text>SearchScreen</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.keyboard} behavior='padding' enabled>
+      <View style={styles.container}>
+        <View
+          style={
+            keyboardShowing
+              ? styles.searchContainerWithKeyboard
+              : styles.searchContainer
+          }
+        >
+          <SearchBox inputHandler={inputHandler} />
+        </View>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -13,5 +41,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#333f50',
+    flexDirection: 'column',
+  },
+  searchContainer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+  },
+  searchContainerWithKeyboard: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+  },
+  keyboard: {
+    flex: 1,
   },
 });
